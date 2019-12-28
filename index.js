ddcf56af4d58c3becf32ed84aefedd1990cea040
ricardosikic <ricardo.sikic@gmail.com>
2019-12-28 21:30:06 +0000

    expresiones de clases

--- index.js
+++ index.js
@@ -23,6 +23,52 @@ let rectangulo_1 = new Rectangulo(14, 20);
 console.log(rectangulo_1.calculaArea());
 
 
+// Ahora veremos Expresiones de clases, que solo es otra forma de definir
+// una clases las cuales pueden ser anonimas o nomradas
+
+// expresion de clase anonima
+let Auto = class {
+  constructor(marca, motor) {
+    this.marca = marca;
+    this.motor = motor;
+  }
+
+  info() {
+    console.log(`la marca es ${this.marca}, tiene motor ${this.motor}`);
+  }
+}
+
+// nueva instancia
+let auto_1 = new Auto('hyundai', 1.7);
+console.log(auto_1.info());
+
+
+// expresion de clase nombrada
+let Car = class Car {
+   constructor(marca, motor) {
+    this.marca = marca;
+    this.motor = motor;
+  }
+
+  info() {
+    console.log(`la marca es ${this.marca}, tiene motor ${this.motor}`);
+  }
+}
+
+let auto_2 = new Car('subaru', 2.5);
+console.log(auto_2.info());
+
+
+// Como pueden ver existen 3 tipos de maneras de poder crear clases en javascript
+// la primera es creando una clase por medio de la palabra reservada class
+// y las otras 2 maneras es por medio de la creacion de expresiones de clases
+
+
+// las cuales pueden ser anonimas:
+// let A = class {}
+
+// como tambien pueden ser nombradas
+// let B = class B {}
 
 
 
@@ -34,31 +80,6 @@ console.log(rectangulo_1.calculaArea());
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// las clases son funciones especiales
 
 // declaro una clase
 class User {
